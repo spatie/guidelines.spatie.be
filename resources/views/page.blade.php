b05eec82b3cacb55c217f65250aad6c367d52012
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-13 14:40:14 +0200

    Background fix

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -4,26 +4,29 @@
     <div class="layout">
         <div class="layout__sidebar">
             <nav class="sidebar">
-                <div class="sidebar__logo">
-                    <a href="https://spatie.be" target="spatie">
-                        @include('svg.logo')
-                    </a>
+                <div class="sidebar__background"></div>
+                <div class="sidebar__contents">
+                    <div class="sidebar__logo">
+                        <a href="https://spatie.be" target="spatie">
+                            @include('svg.logo')
+                        </a>
+                    </div>
+
+                    {{ app('navigation')->menu() }}
+
+                    @if(Auth::check())
+                        <form method="POST" action="{{ route('logout') }}">
+                            {{ csrf_field() }}
+                            <button type="submit" class="sidebar__auth">
+                                Log Out
+                            </button>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="sidebar__auth">
+                            Log In
+                        </a>
+                    @endif
                 </div>
-
-                {{ app('navigation')->menu() }}
-
-                @if(Auth::check())
-                    <form method="POST" action="{{ route('logout') }}">
-                        {{ csrf_field() }}
-                        <button type="submit" class="sidebar__auth">
-                            Log Out
-                        </button>
-                    </form>
-                @else
-                    <a href="{{ route('login') }}" class="sidebar__auth">
-                        Log In
-                    </a>
-                @endif
             </nav>
         </div>
         <div class="layout__main">
