431e5f764dee864ed1f1df8043b5f12a46f4e7bb
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-28 18:21:21 +0200

    Use Laravel Mix

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.config.postCss = [
+    require('postcss-easy-import')(),
+    require('postcss-cssnext')({
+        features: {
+            // Mix takes care of this for us.
+            autoprefixer: false,
+        },
+    }),
+];
+
+mix.js('resources/assets/js/app.js', 'public/js')
+   .postCss('resources/assets/css/app.css', 'public/css');
