edae558a809da5242fa1749d03dddbf1a39365f5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-07-31 09:31:01 +0200

    Homepage tweaks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,17 +13,25 @@
             </div>
         </header>
         <section class="home__introduction">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia excepturi explicabo mollitia ducimus ad maiores voluptatum distinctio et quidem! Molestias ullam autem repellat quia aut nemo porro ab eaque fuga.</p>
+            <p class="-large">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio ultricies, accumsan ipsum non, consequat magna. In ut rutrum lectus.
+            </p>
+            <p>
+                Praesent luctus justo eros, ut ultricies mauris iaculis ac. Vestibulum molestie odio sit amet euismod fermentum. Vivamus vel maximus risus, vel efficitur ligula. In rhoncus nulla velit, eget accumsan mi rutrum eu. Etiam at leo vitae nisl vehicula pharetra. 
+            </p>
+            <p>
+                Vestibulum ultrices velit ut odio cursus, at imperdiet orci rutrum. Nunc eu lacus id nisl mollis sollicitudin. Praesent felis leo, ornare non est et, tincidunt dapibus augue. Sed metus metus, laoreet non porta eget, semper vitae odio.
+            </p>
         </section>
-        <nav class="home__nav">
-            <div class="home__nav__inner">
+        <nav class="home__main">
+            <div class="home__main__inner">
                 {{ app('navigation')->menu()->addClass('menu--home') }}
             </div>
+            <footer class="home__main__footer">
+                <a href="https://spatie.be" target="spatie">
+                    © spatie.be, Antwerp
+                </a>
+            </footer>
         </nav>
-        <footer class="home__credits">
-            <a href="https://spatie.be" target="spatie">
-                © spatie.be, Antwerp
-            </a>
-        </footer>
     </section>
 @endcomponent
