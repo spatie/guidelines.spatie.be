2a296f0e3a0e5dcc265abb9beb9fcc536b8f6d73
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-08-07 18:08:33 +0200

    404 image credits + tweaks

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,8 +4,11 @@
     <div class="error error--404">
         <div class="error__inner">
             <h1 class="error__title">Page not found</h1>
-            <p class="error__subtext">We don't have a guideline for this, <br> you're on your own from here.</p>
+            <p class="error__subtext">We don't have any guidelines for this, <br> looks like you're on your own from here.</p>
             <p class="error__subtext error__subtext--small"><a href="{{ url('/') }}">I'm scared, take me home</a></p>
+        </div>
+        <div class="error__credits">
+            <a href="https://unsplash.com/@sweeticecreamphotography?photo=muxykzQtu1I">Image by Sweet Ice Cream Photography</a>
         </div>
     </div>
 @endcomponent
