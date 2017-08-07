e86d59b6e81c057b9e1458a9c6707a01365f3298
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-08-07 16:23:59 +0200

    404 page; image optimizations

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@component('layouts.app', [
+    'title' => 'Page not found',
+])
+    <div class="error error--404">
+        <div class="error__inner">
+            <h1 class="error__title">Page not found</h1>
+            <p class="error__subtext">We don't have a guideline for this, <br> you're on your own from here.</p>
+            <p class="error__subtext error__subtext--small"><a href="{{ url('/') }}">I'm scared, take me home</a></p>
+        </div>
+    </div>
+@endcomponent
