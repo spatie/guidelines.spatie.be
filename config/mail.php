aeb4fd43d11485cd1ffa69d8daa1f6972c062a1f
Laravel Shift <shift@laravelshift.com>
2019-09-04 13:37:50 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/mail.php
+++ config/mail.php
@@ -11,8 +11,8 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "sendmail", "mailgun", "mandrill", "ses",
-    |            "sparkpost", "log", "array"
+    | Supported: "smtp", "sendmail", "mailgun", "ses",
+    |            "postmark", "log", "array"
     |
     */
 
@@ -37,7 +37,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This is the SMTP port used by your application to deliver e-mails to
-    | users of the application. Like the host, we have set this value to
+    | users of the application. Like the host we have set this value to
     | stay compatible with the Mailgun e-mail application by default.
     |
     */
@@ -119,5 +119,18 @@ return [
             resource_path('views/vendor/mail'),
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | If you are using the "log" driver, you may specify the logging channel
+    | if you prefer to keep mail messages separate from other log entries
+    | for simpler reading. Otherwise, the default channel will be used.
+    |
+    */
+
+    'log_channel' => env('MAIL_LOG_CHANNEL'),
 
 ];
