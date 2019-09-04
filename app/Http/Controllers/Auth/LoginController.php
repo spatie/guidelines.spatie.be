08c8db4d5ef78ee7763f8c4df3ca3329e42789ac
Laravel Shift <shift@laravelshift.com>
2019-09-04 13:37:51 +0000

    Shift cleanup

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Support\Str;
 use Socialite;
 
 class LoginController extends Controller
